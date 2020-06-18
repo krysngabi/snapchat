71e71d1c5c74350dab111a9e5faeeb63cde627d7
Krys <krysngabi@gmail.com>
2020-06-17 21:53:41 -0700

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Krys Ngabi on 6/17/20.
+//  Copyright © 2020 Krys Ngabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
